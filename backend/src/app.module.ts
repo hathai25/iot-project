7ec9e7299e16eb0089492d97a4833a5cbbfbef2a
hathai25 <phamhuyhathai@gmail.com>
2023-12-09 21:00:14 +0700

    add mqtt

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma.service";
 import { AuthModule, UserModule, VehicleModule, AdminModule } from "./modules";
 import { HistoryModule } from "./modules/history/history.module";
+import { MqttService } from "./mqtt.service";
 
 @Module({
   imports: [UserModule, AuthModule, VehicleModule, AdminModule, HistoryModule],
   controllers: [],
-  providers: [PrismaService],
+  providers: [PrismaService, MqttService],
 })
 export class AppModule {}
